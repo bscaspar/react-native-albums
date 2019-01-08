de716ac10c555b1c070413ad95a6f82219de0099
bscaspar <bradyscaspar@outlook.com>
2019-01-08 11:24:31 -0800

    finalized styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <SafeAreaView>
+  <SafeAreaView style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </SafeAreaView>
